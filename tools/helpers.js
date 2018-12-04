a33a76046e6b2815523c5b0e88bcd7e40eaaac37
Hamilton <hamdiggy@gmail.com>
2018-12-03 21:30:28 -0800

    improved qon and qdelegate

--- tools/helpers.js
+++ tools/helpers.js
@@ -1,5 +1,11 @@
+
+
+
 const templater=tf=>oa=>(Array.isArray(oa)?oa:[oa]).reduce((r,o,i,a)=>r+tf(o,i,a),'');
+
 const rebounce=(c,f,a,t=100)=>!c?!setTimeout(()=>f.apply(c,a),t):true;
+
+
 
 const someProps=(p,s)=>o=>p.some(t=>RegExp(s,'i').test(o[t]));
 const somePropsAll=(a,p,s)=>a.filter(someProps(p,s));
@@ -11,13 +17,18 @@ const samePropsAll=(a,p)=>a.filter(sameProps(p));
 const samePropsFirst=(a,p)=>a.find(sameProps(p));
 const samePropsIndex=(a,p)=>a.findIndex(sameProps(p));
 
+
+
 const rand=(n,x)=>{let r=Math.random();return !x?(!n?r<0.5:~~(r*n)):Math.round((r*(x-n))+n)}
+
 const zeros=(n,e)=>(+n+Math.pow(10,e)+'').substr(1);
 
 const numberCommas=n=>(n+'').replace(/\B(?=(?=\d*\.)(\d{3})+(?!\d))/g,',');
 const moneyCommas=(n,d=2)=>'$'+numberCommas(n.toFixed(d));
 
 const arrayReplace=(ax,ar)=>c=>ax.reduce((r,o,i)=>r+r.replace(o,ar[i]),c);
+
+
 
 const readFiles = (f,c,i=0) => {
 	if (f && f[i]) {
@@ -26,6 +37,8 @@ const readFiles = (f,c,i=0) => {
 		reader.readAsDataURL(f[i]);
 	}
 }
+
+
 
 const sift = f => s => s.map(f).filter(o=>o);
 
@@ -36,17 +49,13 @@ const q = (s,sc=document) =>
 const qnext = s => sift(o=>q(o).nextElementSibling)(s);
 const qprev = s => sift(o=>q(o).previousElementSibling)(s);
 const qparent = s => sift(o=>q(o).parentElement)(s);
+
 // Event Delegation Functions
-const qdelegate = el => (es,t,c) =>{
-	es.trim().split(" ").forEach(e =>{
-	    q(el).forEach(o=>o.addEventListener(e,ev=>{
-	        d(t).forEach(to=>{
-	        	if(ev.target==to)c.call(ev.target,ev)
-	    	})
-	    },!1))
-	});
-}
-const qon = o => (et,f) => et.trim().split(" ").forEach(e => q(o).forEach(o=>o.addEventListener(e,f)));
+const qon = sc => (es,fn) =>
+	es.trim().split(/\s+/).forEach(e=>q(sc).forEach(o=>o.addEventListener(e,fn,!1)));
+const qdelegate = sc => (es,sl,fn) =>
+	qon(sc)(es,ev=>d(sl).forEach(to=>if(ev.target==to)fn.call(ev.target,ev)));
+
 
 
 
@@ -57,7 +66,6 @@ export {
 	rand,zeros,
 	numberCommas,moneyCommas,
 	arrayReplace,
-	sift,
-	q,qdelegate,qon,qprev,qnext,qparent,
+	q,delegate,on,qprev,qnext,qparent,
 	readFiles
 };
