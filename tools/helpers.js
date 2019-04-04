eecf188b4979548ba8785beb5ed340983bb0b945
Hamilton <hamdiggy@gmail.com>
2019-04-03 20:42:08 -0700

    pulled out queries

--- tools/helpers.js
+++ tools/helpers.js
@@ -52,53 +52,6 @@ const readFiles = (f,c,i=0) => {
 
 
 
-const getPath = e => e.path || (e.composedPath && e.composedPath());
-const inPath = (ev,to) => getPath(ev).some(o=>o==to);
-const sift = f => s => [...new Set(s.map(f))];
-
-// Selector Function
-const q = (s,sc=document) => 
-    !s || !isDoc(sc) ? [] :
-    s instanceof HTMLElement || s==sc ? [s] : 
-    Array.isArray(s) ? s : sc.querySelectorAll(s);
-
-// Event Delegation Functions
-/*
-example:
-qon('a')('click',e=>console.log(e))
-*/
-const qon = sc => {
-    const d = q(sc);
-    const f = (es,fn) => {
-        es.trim().split(/\s+/).forEach(e=>d.forEach(o=> o.addEventListener(e,fn)));
-        return f;
-    }; 
-    return f;
-}
-/*
-example:
-qdelegate('body')('click','a',e=>console.log(e))
-*/
-const qdelegate = sc => {
-    const d = qon(sc);
-    const f = (es,sl,fn) => {
-        d(es,ev=>q(sl).forEach(to=>inPath(ev,to)?fn.call(to,ev,to):0));
-        return f;
-    };
-    return f;
-}
-
-// Traversal Functions
-const qnext = sift(o=>o.nextElementSibling);
-const qprev = sift(o=>o.previousElementSibling);
-const qparent = sift(o=>o.parentElement);
-
-const qis = (s1,s2) => q(s1).some(o=>o.matches(s2));
-const qclosest = s => sift(o=>o.closest(s));
-
-
-
-
 
 
 export { 
@@ -107,7 +60,6 @@ export {
 	sameProps,samePropsAll,samePropsFirst,samePropsIndex,
 	rand,zeros,
 	numberCommas,moneyCommas,
-	arrayReplace, sift, getPath, inPath,
-	q,qdelegate,qon,qprev,qnext,qparent, qis, qclosest,
+	arrayReplace,
 	readFiles
 };
