8f23d8a57af6362e3e18a6f1a1615b7dd9991b67
Hamilton <hamdiggy@gmail.com>
2019-09-10 11:57:05 -0700

    removed file handling functions for now

--- tools/helpers.php
+++ tools/helpers.php
@@ -1,53 +1,6 @@
 <?php
 
-
-function fixURL($url){
-	$url = preg_replace(
-		array('/http:\/\/[^\/]+/','/\.\./','/\/\//','/\.\//'),
-		array('','','/',''),
-		$url);
-	return $url;
-}
-
-function makeFileFolderList($dir="."){
-	$folder = fixURL($dir);
-	$folders = array();
-	$files = array();
-
-	if(!file_exists($folder)) {
-    	die(json_encode(array(
-    		"error"=>"File Or Folder Does Not Exist",
-    		"dir"=>$dir,
-    		"folder"=>$folder
-    	)));
-	} else
-	if(!is_dir($folder)) {
-		$folder = dirname($folder);
-	}
-
-	if ($handle = opendir($folder)) {
-    	while (false !== ($file = readdir($handle))) {
-    		if($file=="." || $file=="..") continue;
-    		if(is_dir("$folder/$file")) $folders[] = $file;
-    		else $files[] = $file;
-    	}
-
-    	echo json_encode((object) array(
-    		"dir"=>$dir,
-    		"folder"=>$folder,
-    		"self"=>basename($folder),
-    		"parent"=>dirname($folder),
-    		"files"=>$files,
-    		"folders"=>$folders
-    	));
-    	die;
-    } else {
-    	die("error opening directory");
-    }
-}
-
-
-
+// Return array of query results or die with query string and error;
 function getQueryResults($c,$s) {
 	$r = $c->query($s);
 
@@ -73,19 +26,21 @@ function getDefault($g,$p,$s,$d) {
 	)) : $d;
 }
 
-function mapJoin($a,$f,$dj) {
-	return implode($dj, array_map($f, $a));
+// Map $f function onto $a array, and then join using $dj delimiter
+function mapJoin($a,$f,$d) {
+	return implode($d, array_map($f, $a));
 }
-function splitMapJoin($c,$f,$dj,$ds="/\s?,\s?/") {
-	return splitMapJoin(is_array($c)?$c:preg_split($ds,$c),$f,$dj);
+// Test $c value, if string split using $sd delimiter, else use array, and pass values to mapJoin;
+function splitMapJoin($c,$f,$jd,$sd="/\s?,\s?/") {
+	return mapJoin(is_array($c)?$c:preg_split($sd,$c),$f,$jd);
 }
 
-
+// Preformat a print_r output
 function print_p($v) {
 	echo "<pre>",print_r($v),"</pre>";
 }
 
-
+// Make a mysqli connection to database. Return connection object;
 function makeConn($host = "localhost", $user = "", $pass = "", $dbname = "") {
 	$c = new mysqli($host, $user, $pass, $dbname);
 	if($c->connect_errno) die($c->connect_error);
